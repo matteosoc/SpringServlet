683f2afc3998993bdfb8bf97ef30ab01d473457d
Matteo Soccal <soccal.matteo@gmail.com>
2025-01-21 18:43:18 +0100

    servlet mapping

---
+++ src/main/java/com/matteo/HelloServlet.java
@@ -0,0 +1,11 @@
+package com.matteo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) {
+        System.out.println("in service");
+    }
+}
